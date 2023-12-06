8ce6884c41a768ac3bb55f9a26ea4bdc5606149d
severeduck <92829782+severeduck@users.noreply.github.com>
2023-12-06 09:40:51 +0100

    d6

---
+++ day6/app/app Watch App/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app Watch App
+//
+//  Created by Yakiv Kovalskyi on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct app_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
