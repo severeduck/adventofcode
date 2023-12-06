e47b32a34e6597ce9516867a6a0550108a9a6ddd
severeduck <92829782+severeduck@users.noreply.github.com>
2023-12-06 10:00:13 +0100

    d4

---
+++ tasks/day6/aoc_2023_day6UITests/aoc_2023_day6UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  aoc_2023_day6UITestsLaunchTests.swift
+//  aoc_2023_day6UITests
+//
+//  Created by severeduck on 06.12.2023.
+//
+
+import XCTest
+
+final class aoc_2023_day6UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
